c6dbc2ece38355b77d797bf523bff2c1bc7fb338
Nils Homer <nilshomer@gmail.com>
2025-01-30 15:41:51 -0700

    ci: add tests for libbwaindex. force_bytes_with

--- pybwa/__init__.py
+++ pybwa/__init__.py
@@ -1,3 +1,45 @@
 from pybwa.libbwaaln import *  # noqa: F403
 from pybwa.libbwaindex import *  # noqa: F403
 from pybwa.libbwamem import *  # noqa: F403
+import os
+import sysconfig
+
+
+def get_include():
+    """return a list of include directories."""
+    dirname = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+
+    #
+    # Header files may be stored in different relative locations
+    # depending on installation mode (e.g., `python setup.py install`,
+    # `python setup.py develop`. The first entry in each list is
+    # where develop-mode headers can be found.
+    #
+    pybwa_possibilities = [os.path.join(dirname, '..', 'bwa'),
+                            os.path.join(dirname, 'include', 'bwa')]
+
+    includes = [dirname]
+    for header_locations in [pybwa_possibilities]:
+        for header_location in header_locations:
+            if os.path.exists(header_location):
+                includes.append(os.path.abspath(header_location))
+                break
+
+    return includes
+
+
+def get_defines():
+    """return a list of defined compilation parameters."""
+    return []
+
+
+def get_libraries():
+    """return a list of libraries to link against."""
+    # Note that this list does not include libcsamtools.so as there are
+    # numerous name conflicts with libchtslib.so.
+    dirname = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    #pybwa_libs = ['libbwaaln', 'libbwaindex', 'libbwamem']
+    pybwa_libs = ['libbwaindex']
+
+    so = sysconfig.get_config_var('EXT_SUFFIX')
+    return [os.path.join(dirname, x + so) for x in pybwa_libs]
--- tests/test_libbwaindex.py
+++ tests/test_libbwaindex.py
@@ -1,8 +1,36 @@
+import os
+import sysconfig
 from pathlib import Path
 
 import pytest
 
 from pybwa import BwaIndex
+
+so_ext = sysconfig.get_config_var('EXT_SUFFIX')
+so = os.path.join('tests', '_test_libbwaindex' + so_ext)
+
+try:
+    os.unlink("tests/_test_libbwaindex.c")
+    os.unlink("tests/_test_libbwaindex.pyxbldc")
+    os.unlink(so)
+except OSError:
+    pass
+
+NO_PYXIMPORT = False
+try:
+    import pyximport
+    import ctypes
+    retval = pyximport.install(build_in_temp=False, inplace=True)
+    ctypes.cdll.LoadLibrary(so)
+    import _test_libbwaindex
+except Exception as ex:
+    print(f"Cannot import pyximport: {ex}")
+    NO_PYXIMPORT = True
+
+
+@pytest.mark.skipif(NO_PYXIMPORT, reason="no pyximport")
+def test_force_bytes_with() -> None:
+    _test_libbwaindex.test_force_bytes_with()
 
 
 def test_bwa_index_build(e_coli_k12_fasta: Path, tmp_path_factory: pytest.TempPathFactory) -> None:
