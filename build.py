595727ba59e16bd2b145a5e1976d0cdeaaa92cb5
Nils Homer <nilshomer@gmail.com>
2025-03-15 20:58:15 -0700

    try not inplace

--- build.py
+++ build.py
@@ -246,7 +246,7 @@ def build():
         # building the extensions). This is done so Poetry grabs the files
         # during the next step in its build.
         build_ext_cmd.parallel = multiprocessing.cpu_count() * 2
-        build_ext_cmd.inplace = 1
+        #build_ext_cmd.inplace = 1
         build_ext_cmd.run()
 
 
