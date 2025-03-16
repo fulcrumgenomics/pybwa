9d2579be45951656055d8836bfefca72aefdfbe3
Nils Homer <nilshomer@gmail.com>
2025-03-15 20:37:28 -0700

    ruff

--- pybwa/libbwamem.pyi
+++ pybwa/libbwamem.pyi
@@ -7,7 +7,6 @@ from pysam import FastxRecord
 from pybwa.libbwaindex import BwaIndex
 from pybwa.libbwamemopt import BwaMemOptions
 
-
 class BwaMem:
     _index: BwaIndex
     def __init__(self, prefix: str | Path | None = None, index: BwaIndex | None = None) -> None: ...
