e063765f76e462963e245471d058eb69e88c7f55
Martin Larralde <martin.larralde@embl.de>
2025-03-18 12:53:19 +0100

    feat: add function to change verbosity of all libraries

---
+++ pybwa/libbwa.py
@@ -0,0 +1,26 @@
+import enum
+
+from pybwa.libbwaindex import set_bwa_idx_verbosity
+from pybwa.libbwaaln import set_bwa_aln_verbosity
+from pybwa.libbwamem import set_bwa_mem_verbosity
+
+__all__ = [
+    "BwaVerbosity",
+    "set_bwa_verbosity",
+]
+
+# class syntax
+@enum.unique
+class BwaVerbosity(enum.IntEnum):
+    """The verbosity level for the BWA C-API"""
+
+    ERROR = 1
+    WARNING = 2
+    INFO = 3
+    DEBUG = 4
+
+def set_bwa_verbosity(level: BwaVerbosity) -> bool:
+    changed = set_bwa_idx_verbosity(level)
+    changed |= set_bwa_mem_verbosity(level)
+    changed |= set_bwa_aln_verbosity(level)
+    return changed
