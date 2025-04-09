d4fa8fab68845f4a4d573c0d48f970376a6c17d5
Nils Homer <nilshomer@gmail.com>
2025-04-08 18:51:58 -0700

    feat: libdeflate, lzma, curl, and bz2 are now optional
    This PR will run htslib's configure to detect if curl and libdeflate are
    present.  Since lzma and bz2 are by default required by htslib, if
    configure fails, it will be re-run explicitly disabling support for
    these one-by-one.
    
    Logging in the build file is also improved.

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
+import dataclasses
+import logging
 import multiprocessing
 import os
 import platform
+import re
 import shutil
 import subprocess
 from contextlib import contextmanager
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 from typing import List
@@ -13,6 +17,35 @@ from Cython.Build import cythonize
 from Cython.Distutils.build_ext import new_build_ext as cython_build_ext
 from setuptools import Distribution
 from setuptools import Extension
+
+
+@dataclass
+class HtslibConfig:
+    """Stores library config as inferred from htslib's config.h."""
+
+    has_libbz2: bool = False
+    has_libcurl: bool = False
+    has_libdeflate: bool = False
+    has_liblzma: bool = False
+
+    @classmethod
+    def from_htslib_config_h(cls, config_h: Path) -> "HtslibConfig":
+        """Builds an instance from htslib's config.h."""
+        this: HtslibConfig = HtslibConfig()
+        with config_h.open("r") as reader:
+            for line in reader:
+                if not line.startswith("#define"):
+                    continue
+                key, value = re.match(r"#define (\S+)\s+(\S+)", line).groups()
+                if key == "HAVE_LIBBZ2":
+                    this = dataclasses.replace(this, has_libbz2=(value == "1"))
+                elif key == "HAVE_LIBCURL":
+                    this = dataclasses.replace(this, has_libcurl=(value == "1"))
+                elif key == "HAVE_LIBDEFLATE":
+                    this = dataclasses.replace(this, has_libdeflate=(value == "1"))
+                elif key == "HAVE_LIBLZMA":
+                    this = dataclasses.replace(this, has_liblzma=(value == "1"))
+        return this
 
 
 def strtobool(value: str) -> bool:
@@ -42,31 +75,54 @@ USE_GIT: bool = shutil.which("git") is not None and Path(".git").exists() and Pa
 IS_DARWIN = platform.system() == "Darwin"
 
 
-def compile_htslib() -> None:
+def run_command(command: str, fail_on_error: bool = True) -> int:
+    """Runs a given command, return the return code or failing on error if desired."""
+    retcode = subprocess.call(
+        f"{command}", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True
+    )
+    if fail_on_error and retcode != 0:  # run again for debugging
+        subprocess.call(f"{command}", shell=True)
+        raise RuntimeError(f"Failed to run command {command}")
+    return retcode
+
+
+def compile_htslib(logger: logging.Logger) -> None:
     """Complies htslib prior to entering the context."""
-    print("Building htslib...")
     with changedir("htslib"):
         cflags = "CFLAGS='-fpic -fvisibility=hidden -g -Wall -O2"
         if IS_DARWIN:
             cflags += " -mmacosx-version-min=11.0"
         cflags += "'"
-        commands = [
-            "autoreconf -i",
-            f"./configure --with-libdeflate --enable-lzma --enable-bz2 {cflags}",
-            "make -j",
+
+        # regenerate GNU build files
+        logger.info("Regenerating htslib build files...")
+        run_command(command="autoreconf -i")
+
+        # run configure, trying to disable bz2 and lzma when it fails
+        logger.info("Configuring htslib...")
+        configure_options = [
+            "",  # all features
+            "--disable-bz2",  # no bz2
+            "--disable-lzma",  # no lzma
+            "--disable-bz2 --disable-lzma",  # no bz2 and no lzma
         ]
-        for command in commands:
-            retcode = subprocess.call(
-                f"{command}", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True
-            )
-            if retcode != 0:  # run again for debugging
-                retcode = subprocess.call(f"{command}", shell=True)
-            if retcode != 0:
-                raise RuntimeError(f"Failed to run command {command}")
+        for options in configure_options:
+            if options != "":
+                logger.warning(f"... retrying with configure options [{options}]")
+            command = f"./configure {options} {cflags}"
+            if run_command(command=command, fail_on_error=False) == 0:
+                break
+        else:  # run one last time to failure
+            command = f"./configure {cflags}"
+            run_command(command=command)
+
+        # build it
+        logger.info("Building htslib...")
+        run_command(command="make -j")
 
 
 @contextmanager
-def with_patches() -> Any:
+def with_patches(logger: logging.Logger) -> Any:
     """Applies patches to bwa, then cleans up after exiting the context."""
     patches = sorted([
         os.path.abspath(patch)
@@ -74,7 +130,7 @@ def with_patches() -> Any:
         if patch.is_file() and patch.suffix == ".patch"
     ])
     with changedir("bwa"):
-        print("Patching bwa...")
+        logger.info("Patching bwa...")
         for patch in patches:
             if USE_GIT:
                 retcode = subprocess.call(f"git apply --whitespace=nowarn {patch}", shell=True)
@@ -86,6 +142,7 @@ def with_patches() -> Any:
         yield
     finally:
         if USE_GIT:
+            logger.info("Restoring bwa...")
             commands = ["git submodule deinit -f bwa", "git submodule update --init --recursive"]
             for command in commands:
                 retcode = subprocess.call(command, shell=True)
@@ -102,7 +159,7 @@ BUILD_DIR = Path("cython_build")
 compile_args: list[str] = []
 link_args: list[str] = []
 include_dirs = ["htslib", "bwa", "pybwa", os.path.dirname(pysam.__file__)]
-libraries = ["m", "z", "pthread", "lzma", "bz2", "curl", "deflate"]
+libraries = ["m", "z", "pthread"]
 if platform.system() == "Linux":
     libraries.append("rt")
 library_dirs = ["pybwa", "bwa", "htslib"]
@@ -156,48 +213,6 @@ if platform.system() != "Windows":
         "-Wno-macro-redefined",
     ])
 
-libbwaindex_module = Extension(
-    name="pybwa.libbwaindex",
-    sources=["pybwa/libbwaindex.pyx"] + c_files,
-    depends=h_files,
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
-    extra_objects=extra_objects,
-    include_dirs=include_dirs,
-    language="c",
-    libraries=libraries,
-    library_dirs=library_dirs,
-    define_macros=define_macros,
-)
-
-libbwaaln_module = Extension(
-    name="pybwa.libbwaaln",
-    sources=["pybwa/libbwaaln.pyx"] + c_files,
-    depends=h_files,
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
-    extra_objects=extra_objects,
-    include_dirs=include_dirs,
-    language="c",
-    libraries=libraries,
-    library_dirs=library_dirs,
-    define_macros=define_macros,
-)
-
-libbwamem_module = Extension(
-    name="pybwa.libbwamem",
-    sources=["pybwa/libbwamem.pyx"] + c_files,
-    depends=h_files,
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
-    extra_objects=extra_objects,
-    include_dirs=include_dirs,
-    language="c",
-    libraries=libraries,
-    library_dirs=library_dirs,
-    define_macros=define_macros,
-)
-
 
 def cythonize_helper(extension_modules: List[Extension]) -> Any:
     """Cythonize all Python extensions."""
@@ -239,12 +254,86 @@ CLASSIFIERS = [
 
 def build() -> None:
     """The main build function for pybwa."""
+    # set up logging
+    logging.basicConfig(
+        format="%(asctime)s [%(levelname)s]: %(message)s",
+    )
+    logger = logging.getLogger("pybwa build")
+    logger.setLevel("INFO")
+
     # compile htslib
-    compile_htslib()
+    compile_htslib(logger=logger)
 
     # apply patches to bwa, then revert them after
-    with with_patches():
-        print("Building extensions...")
+    with with_patches(logger=logger):
+        # get the configuration from htslib
+        logger.info("Setting pybwa configuration...")
+        config = HtslibConfig.from_htslib_config_h(Path("htslib/config.h"))
+        if config.has_libbz2:
+            logger.info("... building **with** bz2 support")
+            libraries.append("bz2")
+        else:
+            logger.warning("... building **without** bz2 support")
+        if config.has_libcurl:
+            logger.info("... building **with** curl support")
+            libraries.append("curl")
+        else:
+            logger.warning("... building **without** curl support")
+        if config.has_libdeflate:
+            logger.info("... building **with** libdeflate support")
+            libraries.append("deflate")
+        else:
+            logger.warning("... building **without** libdeflate support")
+        if config.has_liblzma:
+            logger.info("... building **with** lzma support")
+            libraries.append("lzma")
+        else:
+            logger.warning("... building **without** lzma support")
+
+        # Define the extension modules
+        libbwaindex_module = Extension(
+            name="pybwa.libbwaindex",
+            sources=["pybwa/libbwaindex.pyx"] + c_files,
+            depends=h_files,
+            extra_compile_args=compile_args,
+            extra_link_args=link_args,
+            extra_objects=extra_objects,
+            include_dirs=include_dirs,
+            language="c",
+            libraries=libraries,
+            library_dirs=library_dirs,
+            define_macros=define_macros,
+        )
+
+        libbwaaln_module = Extension(
+            name="pybwa.libbwaaln",
+            sources=["pybwa/libbwaaln.pyx"] + c_files,
+            depends=h_files,
+            extra_compile_args=compile_args,
+            extra_link_args=link_args,
+            extra_objects=extra_objects,
+            include_dirs=include_dirs,
+            language="c",
+            libraries=libraries,
+            library_dirs=library_dirs,
+            define_macros=define_macros,
+        )
+
+        libbwamem_module = Extension(
+            name="pybwa.libbwamem",
+            sources=["pybwa/libbwamem.pyx"] + c_files,
+            depends=h_files,
+            extra_compile_args=compile_args,
+            extra_link_args=link_args,
+            extra_objects=extra_objects,
+            include_dirs=include_dirs,
+            language="c",
+            libraries=libraries,
+            library_dirs=library_dirs,
+            define_macros=define_macros,
+        )
+
+        logger.info("Cythonize extensions...")
         # Collect and cythonize all files
         extension_modules = cythonize_helper([
             libbwaindex_module,
@@ -296,11 +385,12 @@ def build() -> None:
         # during the next step in its build.
         build_ext_cmd.parallel = strtobool(os.environ.get("BUILD_EXTENSIONS_PARALLEL", "True"))
         if build_ext_cmd.parallel:
-            print("Building cython extensions in parallel")
+            logger.info("Building cython extensions in parallel...")
         else:
-            print("Building cython extensions serially")
+            logger.info("Building cython extensions serially...")
         build_ext_cmd.inplace = True
         build_ext_cmd.run()
+    logger.info("Build successful!")
 
 
 if __name__ == "__main__":
